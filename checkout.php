7806a8c7bd0ed104daeaf5424d945d2a2849c45c
aharr351 <152522604+aharr351@users.noreply.github.com>
2024-02-28 06:00:28 -0500

    error

--- checkout.php
+++ checkout.php
@@ -4,13 +4,13 @@
     <title>Inser Checkout Data</title>
 </head>
 <body>
-        <?php
+       <?php
         session_start();
         $DATABASE_HOST ='localhost';
         $DATABASE_USER = 'root';
         $DATABASE_PASS = '';
-        $DATABASE_NAME = 'test'
-        $conn = mysqli_connect($DATABASE_HOST, $DATABASE_USER,$DATABASE_PASS,$DATABASE_NAME)
+        $DATABASE_NAME = 'test';
+        $conn = mysqli_connect($DATABASE_HOST, $DATABASE_USER,$DATABASE_PASS,$DATABASE_NAME);
         if (!$conn){
             die("Conection failed:".mysqli_connect_error());
         }
