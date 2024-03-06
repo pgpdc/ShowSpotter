6c2acf072c4fc84050598bd8c31279c009e41ec5
autumn <ztsz@iup.edu>
2024-03-06 11:12:17 -0500

    changed back to orignial

--- room01/seatsave.php
+++ room01/seatsave.php
@@ -1,20 +1,7 @@
-<!DOCTYPE html>
-<html>
-<body>
 <?php
 // (A) LOAD LIBRARY
 require "seatreservelib.php";
 
 // (B) SAVE
-
 $_RSV->save($_POST["sessid"], $_POST["userid"], $_POST["seats"]);
-?>
-
-<form method="POST" action="checkoutForm.php">
-    How many adults:<input type="text" name="adult"><br>
-      <input type="hidden" name="sessid" value="<?=$sessid?>">
-      <input type="hidden" name="userid" value="<?=$userid?>">
-</form>
-
-</body>
-</html>
+echo "SAVED";
--- room01/seatsaveOriginal.php
+++
@@ -1,7 +0,0 @@
-<?php
-// (A) LOAD LIBRARY
-require "seatreservelib.php";
-
-// (B) SAVE
-$_RSV->save($_POST["sessid"], $_POST["userid"], $_POST["seats"]);
-echo "SAVED";
