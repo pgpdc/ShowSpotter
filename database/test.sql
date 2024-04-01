95e39a9a50dd67252b37484e0df0bfb7f8ea0e20
alex <pgpdc@iup.edu>
2024-04-01 12:49:06 -0400

    test datebase for date and time selections

---
+++ database/test.sql
@@ -0,0 +1,290 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 01, 2024 at 06:48 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `test`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `paymentinfo`
+--
+
+CREATE TABLE `paymentinfo` (
+  `username` varchar(20) DEFAULT NULL,
+  `password` varchar(30) DEFAULT NULL,
+  `cardName` varchar(50) NOT NULL,
+  `creditNum` varchar(20) NOT NULL,
+  `expDate` varchar(10) NOT NULL,
+  `cvv` int(5) NOT NULL,
+  `name` text NOT NULL,
+  `address` varchar(40) NOT NULL,
+  `city` text NOT NULL,
+  `state` text NOT NULL,
+  `zipCode` int(10) NOT NULL,
+  `billSame` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `paymentinfo`
+--
+
+INSERT INTO `paymentinfo` (`username`, `password`, `cardName`, `creditNum`, `expDate`, `cvv`, `name`, `address`, `city`, `state`, `zipCode`, `billSame`) VALUES
+('', '', '', '', '', 0, '', '', '', '', 0, ''),
+('', '', '', '', '', 0, '', '', '', '', 0, ''),
+('', '', '', '', '', 0, '', '', '', '', 0, ''),
+('test3', 'f54', 'fsdfsdf', '4324324', '324324', 432, 'sfsfsd', '', 'were', 'rewr', 4334, 'yes'),
+('', '', '', '', '', 0, '', '', '', '', 0, ''),
+('AGar', 'djkwd', 'Graham Smith', '1656-3747-4843-3333', '12/23', 432, 'Graham Smith', '3333 Street', 'City', 'PA', 27463, 'yes'),
+('', '', '', '', '', 0, '', '', '', '', 0, '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `prices`
+--
+
+CREATE TABLE `prices` (
+  `ticket` varchar(30) NOT NULL,
+  `cost` decimal(30,0) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `prices`
+--
+
+INSERT INTO `prices` (`ticket`, `cost`) VALUES
+('Adult', 9),
+('Child', 6),
+('Senior', 4);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reservations`
+--
+
+CREATE TABLE `reservations` (
+  `session_id` bigint(20) NOT NULL,
+  `seat_id` varchar(16) NOT NULL,
+  `user_id` bigint(20) NOT NULL,
+  `time` varchar(32) NOT NULL,
+  `date` varchar(16) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `reservations`
+--
+
+INSERT INTO `reservations` (`session_id`, `seat_id`, `user_id`, `time`, `date`) VALUES
+(1, 'C5', 999, '21:00:00', '2024-04-02'),
+(1, 'E2', 999, '21:00:00', '2024-04-01');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reservedtimes`
+--
+
+CREATE TABLE `reservedtimes` (
+  `type` varchar(20) NOT NULL,
+  `ticket` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `reservedtimes`
+--
+
+INSERT INTO `reservedtimes` (`type`, `ticket`) VALUES
+('A1', 'Adult'),
+('A2', 'Senior'),
+('A3', 'Child'),
+('A6', 'Adult'),
+('B2', 'Adult'),
+('C2', 'Adult'),
+('C4', 'Adult'),
+('C5', 'Adult'),
+('C6', 'Adult'),
+('C7', 'Adult'),
+('C8', 'Child'),
+('D1', 'Adult'),
+('D3', 'Adult'),
+('D8', 'Senior'),
+('E1', 'Adult'),
+('E2', 'Adult'),
+('E5', 'Adult'),
+('E6', 'Senior'),
+('F3', 'Adult'),
+('F6', 'Adult'),
+('F7', 'Child'),
+('F8', 'Senior'),
+('G7', 'Adult'),
+('G8', 'Senior');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `seats`
+--
+
+CREATE TABLE `seats` (
+  `seat_id` varchar(16) NOT NULL,
+  `room_id` varchar(16) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `seats`
+--
+
+INSERT INTO `seats` (`seat_id`, `room_id`) VALUES
+('A1', '01'),
+('A2', '01'),
+('A3', '01'),
+('A4', '01'),
+('A5', '01'),
+('A6', '01'),
+('A7', '01'),
+('A8', '01'),
+('A9', '01'),
+('B1', '01'),
+('B2', '01'),
+('B3', '01'),
+('B4', '01'),
+('B5', '01'),
+('B6', '01'),
+('B7', '01'),
+('B8', '01'),
+('B9', '01'),
+('C1', '01'),
+('C2', '01'),
+('C3', '01'),
+('C4', '01'),
+('C5', '01'),
+('C6', '01'),
+('C7', '01'),
+('C8', '01'),
+('C9', '01'),
+('D1', '01'),
+('D2', '01'),
+('D3', '01'),
+('D4', '01'),
+('D5', '01'),
+('D6', '01'),
+('D7', '01'),
+('D8', '01'),
+('D9', '01'),
+('E1', '01'),
+('E2', '01'),
+('E3', '01'),
+('E4', '01'),
+('E5', '01'),
+('E6', '01'),
+('E7', '01'),
+('E8', '01'),
+('E9', '01'),
+('F1', '01'),
+('F2', '01'),
+('F3', '01'),
+('F4', '01'),
+('F5', '01'),
+('F6', '01'),
+('F7', '01'),
+('F8', '01'),
+('F9', '01'),
+('G1', '01'),
+('G2', '01'),
+('G3', '01'),
+('G4', '01'),
+('G5', '01'),
+('G6', '01'),
+('G7', '01'),
+('G8', '01'),
+('G9', '01');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sessions`
+--
+
+CREATE TABLE `sessions` (
+  `session_id` bigint(20) NOT NULL,
+  `room_id` varchar(16) NOT NULL,
+  `session_date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `sessions`
+--
+
+INSERT INTO `sessions` (`session_id`, `room_id`, `session_date`) VALUES
+(1, '01', '2077-06-05 08:00:00');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `prices`
+--
+ALTER TABLE `prices`
+  ADD KEY `ticket` (`ticket`,`cost`);
+
+--
+-- Indexes for table `reservations`
+--
+ALTER TABLE `reservations`
+  ADD PRIMARY KEY (`seat_id`,`time`,`date`);
+
+--
+-- Indexes for table `reservedtimes`
+--
+ALTER TABLE `reservedtimes`
+  ADD PRIMARY KEY (`type`),
+  ADD KEY `type` (`type`,`ticket`);
+
+--
+-- Indexes for table `seats`
+--
+ALTER TABLE `seats`
+  ADD PRIMARY KEY (`seat_id`,`room_id`);
+
+--
+-- Indexes for table `sessions`
+--
+ALTER TABLE `sessions`
+  ADD PRIMARY KEY (`session_id`),
+  ADD KEY `room_id` (`room_id`),
+  ADD KEY `session_date` (`session_date`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `sessions`
+--
+ALTER TABLE `sessions`
+  MODIFY `session_id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
