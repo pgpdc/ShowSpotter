a1b1b83052e0c8d8011665b86878035d14ce023a
autumn <ztsz@iup.edu>
2024-04-03 13:02:55 -0400

    just add option value 3 because the dropdown was missing it

--- room01/concessions.php
+++ room01/concessions.php
@@ -71,6 +71,7 @@ session_start();
               <option value='0'></option>
               <option value='1'>1</option>
               <option value='2'>2</option>
+              <option value='3'>3</option>
               <option value='4'>4</option>
               <option value='5'>5</option>
               <option value='6'>6</option>
