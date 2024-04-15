95648230008062f7d389fc3fdad5ddee4aabfd0c
alex <pgpdc@iup.edu>
2024-04-15 14:48:38 -0400

    one more db change

--- modifyInventory.php
+++ modifyInventory.php
@@ -8,7 +8,7 @@ if ($_SESSION["admin"] != TRUE) {
 $DATABASE_HOST = 'localhost';
 $DATABASE_USER = 'root';
 $DATABASE_PASS = '';
-$DATABASE_NAME = 'test';
+$DATABASE_NAME = 'indiana';
 
 $link = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 if (mysqli_connect_errno()) {
