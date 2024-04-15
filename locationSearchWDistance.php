74838a4fbc2a32e95f1926c987692c471ab9dd48
GabeTarn <gabrielhieu@gmail.com>
2024-04-15 14:56:08 -0400

    Addresses
    Works

---
+++ locationSearchWDistance.php
@@ -0,0 +1,59 @@
+<?php
+function fetchLocations($latitude, $longitude, $distance) {
+    $postData = json_encode([
+        "includedTypes" => ["movie_theater"],
+        "maxResultCount" => 10,
+        "rankPreference" => "DISTANCE",
+        "locationRestriction" => [
+            "circle" => [
+                "center" => ["latitude" => $latitude, "longitude" => $longitude],
+                "radius" => $distance
+            ]
+        ]
+    ]);
+
+    $apiUrl = "https://places.googleapis.com/v1/places:searchNearby";
+    $headers = [
+        "Content-Type: application/json",
+        "X-Goog-Api-Key: AIzaSyCZa8k5Xckx5xLtJkdv3W5HkhV7OKd6CC0", 
+        "X-Goog-FieldMask: places.displayName,places.formattedAddress",
+    ];
+
+    $ch = curl_init($apiUrl);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+    $response = curl_exec($ch);
+    $err = curl_error($ch);
+    curl_close($ch);
+
+    if ($err) {
+        return "cURL Error #:" . $err;
+    } else {
+        return json_decode($response, true);
+    }
+    return [
+        'status' => 'success',
+        'message' => "Received latitude: $latitude, longitude: $longitude"
+    ];
+}
+
+$locationsData = [];
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    
+    $latitude = filter_input(INPUT_POST, 'latitude', FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+    $longitude = filter_input(INPUT_POST, 'longitude', FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+    $distance = filter_input(INPUT_POST, 'distance', FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+
+    $result = fetchLocations($latitude, $longitude, $distance); 
+
+    header('Content-Type: application/json');
+    $response = ['status' => 'success', 'message' => 'Data processed successfully.'];
+    echo json_encode($result);
+    
+    exit;
+}
+?>
