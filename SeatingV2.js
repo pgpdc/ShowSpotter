c2220e050b791829c1a9de270bcee761dff651a6
NicoMMGZ <159072113+NicoMMGZ@users.noreply.github.com>
2024-04-17 10:23:27 -0400

    Add files via upload

--- SeatingV2.js
+++ SeatingV2.js
@@ -1,10 +1,26 @@
-const container = document.querySelector(".container");
+var reserve = {
+  // (A) CHOOSE THIS SEAT
+  toggle : seat => seat.classList.toggle("seat"),
 
-container.addEventListener("click", (e) => {
-  if (
-    e.target.classList.contains("seat") &&
-    !e.target.classList.contains("sold")
-  ) {
-    e.target.classList.toggle("selected");
+  // (B) SAVE RESERVATION
+  save : () => {
+    // (B1) GET SELECTED SEATS
+    var selected = document.querySelectorAll("#layout .selected");
+
+    // (B2) ERROR!
+    if (selected.length == 0) { alert("No seats selected."); }
+
+    // (B3) NINJA FORM SUBMISSION
+    else {
+      var ninja = document.getElementById("ninja");
+      for (let seat of selected) {
+        let input = document.createElement("input");
+        input.type = "hidden";
+        input.name = "seats[]";
+        input.value = seat.innerHTML;
+        ninja.appendChild(input);
+      }
+      ninja.submit();
+    }
   }
-});
+};
