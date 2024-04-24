660286d58de558dd9a07ed5f5c79e2f821dd110a
Nick <MMGZ@iup.edu>
2024-04-23 22:16:06 -0400

    updated seat format hopefully working with new system

--- room01/SeatingV2.php
+++ room01/SeatingV2.php
@@ -11,14 +11,25 @@
   <body>
 
   <?php
+  session_start();
     // (A) FIXED IDS FOR THIS DEMO
     $sessid = 1;
     $userid = 999;
     $count = 0;
+    $date = $_GET["date"];
+    $time = $_GET["time"];
+    $id = $_GET["id"];
+    echo $date;
+    echo "\n";
+    echo $time;
+    echo "\n";
+    echo "room";
+    echo $id;
+    echo "Name " . $_SESSION["name"];
 
     // (B) GET SESSION SEATS
     require "seatreservelib.php";
-    $seats = $_RSV->get($sessid);
+    $seats = $_RSV->get($sessid, $time, $date, $id);
     ?>
 
     <ul class="showcase">
@@ -46,12 +57,13 @@
           printf("<div class='row'>");
         }
         $count +=1; 
-        $sold = is_numeric($s["user_id"]);
-        printf("<div class='seat%s'%s>%s</div>",
-          $sold ? " sold" : "",
-          $sold ? "" : " onclick='reserve.toggle(this)'",
-          $s["seat_id"]
-        );
+        if ($s["time"] === $time) {
+          // Seat is taken
+          printf("<div class='seat taken'>%s</div>", $s["seat_id"]);
+      } else {
+          // Seat is available
+          printf("<div class='seat' onclick='reserve.toggle(this)'>%s</div>", $s["seat_id"]);
+      }
       }
       ?>
     </div>
