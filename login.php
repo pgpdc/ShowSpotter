a6df8f9f7ca27734f38e849e6f5267d430c3c05f
Nick <MMGZ@iup.edu>
2024-04-29 14:38:26 -0400

    testing the login css

--- login.php
+++ login.php
@@ -67,6 +67,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <title>Login</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="Styles/navbar.css">
+    <link rel="stylesheet" href="login.css">
     <style>
         body{ font: 14px sans-serif; }
         .wrapper{ width: 360px; padding: 20px; }
