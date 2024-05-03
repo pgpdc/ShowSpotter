69273609a69e497375a1568a89629e3aac9ed5d4
Bryce <qybdc@iup.edu>
2024-05-03 15:00:01 -0400

    Fixed Navbar

--- navbar.php
+++ navbar.php
@@ -22,7 +22,9 @@
     <?php if (isset($_SESSION['admin'])) : ?>
         <a href="/ShowSpotter/admin/admin.php">Admin Hub</a>
     <?php elseif (isset($_SESSION['loggedin'])) : ?>
-        <a href="/ShowSpotter/customer/customer.php">Account Hub</a>
+        <?php if ($_SERVER['PHP_SELF'] !== '/ShowSpotter/index.php'): ?>
+            <a href="/ShowSpotter/customer/customer.php">Account Hub</a>
+            <?php endif; ?>
     <?php endif; ?>
     <div class="dropdown">
         <?php if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] == true) : ?>
